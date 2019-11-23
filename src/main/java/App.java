82947dcf0cdb145feab478fbe0e183ed82b77ce9
liorpiz <liorpezman@gmail.com>
2019-11-23 15:17:41 +0200

    count specific string command

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+
+public class App {
+
+    private WebCrawler webCrawler;
+    private Commands commands;
+
+    public App() {
+        this.webCrawler = new WebCrawler();
+        this.commands = new Commands(this.webCrawler.getNames());
+        /* test cases */
+//        LinkedList<String> test = new LinkedList<String>();
+//        test.add("Assaf");
+//        test.add("YaAsSrden");
+//        this.commands = new Commands(test);
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.commands.CountSpecificString("write-your-substring");
+    }
+}
