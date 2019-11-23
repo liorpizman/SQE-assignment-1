a0b80b9247774f6820c49783dfc05e62b212145d
liorpiz <liorpezman@gmail.com>
2019-11-23 16:07:02 +0200

    count all strings

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 public class Commands {
 
     private LinkedList<String> names;
+    private HashMap<String, Integer> stringsMap;
 
     public Commands(LinkedList<String> names) {
         this.names = names;
@@ -23,12 +27,43 @@ public class Commands {
     }
 
     /**
-     * Command 2 -
+     * Command 2 - counts all strings in a chosen size of n the names' list
      *
-     * @param length
+     * @param length - chosen size
      */
     public void CountAllStrings(int length) {
-        System.out.println();
+        this.stringsMap = new HashMap<String, Integer>();
+        for (String name : names) {
+            countSubstringsInstances(name, length);
+        }
+        printByStructure();
+    }
+
+    /**
+     * counts all substring in the chosen size in a current string
+     *
+     * @param name current string in names' list
+     * @param size chosen size
+     */
+    public void countSubstringsInstances(String name, int size) {
+        for (int i = 0; (i + size) <= name.length(); i++) {
+            String substring = name.substring(i, i + size);
+            Integer count = this.stringsMap.get(substring);
+            if (count == null) {
+                this.stringsMap.put(substring, 1);
+            } else {
+                this.stringsMap.put(substring, count + 1);
+            }
+        }
+    }
+
+    /**
+     * prints all substrings in names' list and the number of times they appeared
+     */
+    public void printByStructure() {
+        for (String substring : this.stringsMap.keySet()) {
+            System.out.println(substring + ':' + this.stringsMap.get(substring));
+        }
     }
 
     /**
